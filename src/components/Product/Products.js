df90581efccb84e81ea20073540e514d07349c5c
Saifudin Badani <87968258+saifudinbadani@users.noreply.github.com>
2022-03-21 14:25:18 +0800

    add: images compiled in a file andproduct card refactored

--- src/components/Product/Products.js
+++ src/components/Product/Products.js
@@ -1,5 +1,6 @@
 import '../../css/product-listing.css';
-import diningHomePage from '../../assets/dining-homepage.jpg';
+
+import { products } from '../../backend/db/products';
 
 const Product = () => {
     return <main class="products-listing-container p-rl-6">
@@ -9,115 +10,26 @@ const Product = () => {
             products )</span>
     </h4>
     <div class="products-card-container display-flex">
-        <div class="card-h display-flex">
-            <div class="card-img-h">
-                <img src={diningHomePage} alt="Dining"/>
-            </div>
-            <div class="card-content-h display-flex-c">
-                <h3 class="card-title heading-3 fw-regular">Atrium Buffet</h3>
-                <small class="card-author">by Hotelaza</small>
-                <p class="text-black heading-4 fw-bold">$600 <s class="card-text heading-4 fw-regular">$850</s>
-                </p>
-                <div class="card-footer-h">
-                    <button class="card-btn btn btn-solid-primary font-size-1pt4 width-100pcnt">Add to
-                        Cart</button>
-                    <button class="card-btn btn btn-outline-secondary font-size-1pt4 width-100pcnt">Move to
-                        Wishlist</button>
-                </div>
-            </div>
-        </div>
-
-        <div class="card-h display-flex">
-            <div class="card-img-h">
-                <img src={diningHomePage} alt="Dining"/>
-            </div>
-            <div class="card-content-h display-flex-c">
-                <h3 class="card-title heading-3 fw-regular">Atrium Buffet</h3>
-                <small class="card-author">by Hotelaza</small>
-                <p class="text-black heading-4 fw-bold">$600 <s class="card-text heading-4 fw-regular">$850</s>
-                </p>
-                <div class="card-footer-h">
-                    <button class="card-btn btn btn-solid-primary font-size-1pt4 width-100pcnt">Add to
-                        Cart</button>
-                    <button class="card-btn btn btn-outline-secondary font-size-1pt4 width-100pcnt">Move to
-                        Wishlist</button>
-                </div>
-            </div>
-        </div>
-
-        <div class="card-h display-flex">
-            <div class="card-img-h">
-                <img src={diningHomePage} alt="Dining"/>
-            </div>
-            <div class="card-content-h display-flex-c">
-                <h3 class="card-title heading-3 fw-regular">Atrium Buffet</h3>
-                <small class="card-author">by Hotelaza</small>
-                <p class="text-black heading-4 fw-bold">$600 <s class="card-text heading-4 fw-regular">$850</s>
-                </p>
-                <div class="card-footer-h">
-                    <button class="card-btn btn btn-solid-primary font-size-1pt4 width-100pcnt">Add to
-                        Cart</button>
-                    <button class="card-btn btn btn-outline-secondary font-size-1pt4 width-100pcnt">Move to
-                        Wishlist</button>
-                </div>
-            </div>
-        </div>
-
-        <div class="card-h display-flex">
-            <div class="card-img-h">
-                <img src={diningHomePage} alt="Dining"/>
-            </div>
-            <div class="card-content-h display-flex-c">
-                <h3 class="card-title heading-3 fw-regular">Atrium Buffet</h3>
-                <small class="card-author">by Hotelaza</small>
-                <p class="text-black heading-4 fw-bold">$600 <s class="card-text heading-4 fw-regular">$850</s>
-                </p>
-                <div class="card-footer-h">
-                    <button class="card-btn btn btn-solid-primary font-size-1pt4 width-100pcnt">Add to
-                        Cart</button>
-                    <button class="card-btn btn btn-outline-secondary font-size-1pt4 width-100pcnt">Move to
-                        Wishlist</button>
-                </div>
-            </div>
-        </div>
-
-        <div class="card-h display-flex">
-            <div class="card-img-h">
-                <img src={diningHomePage} alt="Dining"/>
-            </div>
-            <div class="card-content-h display-flex-c">
-                <h3 class="card-title heading-3 fw-regular">Atrium Buffet</h3>
-                <small class="card-author">by Hotelaza</small>
-                <p class="text-black heading-4 fw-bold">$600 <s class="card-text heading-4 fw-regular">$850</s>
-                </p>
-                <div class="card-footer-h">
-                    <button class="card-btn btn btn-solid-primary font-size-1pt4 width-100pcnt">Add to
-                        Cart</button>
-                    <button class="card-btn btn btn-outline-secondary font-size-1pt4 width-100pcnt">Move to
-                        Wishlist</button>
-                </div>
-            </div>
-        </div>
-
-        <div class="card-h display-flex">
-            <div class="card-img-h">
-                <img src={diningHomePage} alt="Dining"/>
-            </div>
-            <div class="card-content-h display-flex-c">
-                <h3 class="card-title heading-3 fw-regular">Atrium Buffet</h3>
-                <small class="card-author">by Hotelaza</small>
-                <p class="text-black heading-4 fw-bold">$600 <s class="card-text heading-4 fw-regular">$850</s>
-                </p>
-                <div class="card-footer-h">
-                    <button class="card-btn btn btn-solid-primary font-size-1pt4 width-100pcnt">Add to
-                        Cart</button>
-                    <button class="card-btn btn btn-outline-secondary font-size-1pt4 width-100pcnt">Move to
-                        Wishlist</button>
-                </div>
-            </div>
-        </div>
+        {products.map(item => {
+            return  <div class="card-h display-flex">
+                       <div class="card-img-h pos-rltv">
+                             <img src={item.img} alt="Dining"/>
+                             <i class="card-dismiss fa-regular fa-heart"></i>
+                         </div>
+                         <div class="card-content-h display-flex-c">
+                            <h3 class="card-title heading-4 fw-regular">{item.title}</h3>
+                             <small class="card-author heading-5">{item.description}</small>
+                             <p class="text-black heading-4 fw-bold">${item.price}</p>
+                            <div class="card-footer-h">
+                                 <button class="card-btn btn btn-solid-primary font-size-1pt4 width-100pcnt">Add to
+                                    Cart</button>
+                             </div>
+                         </div>
+                     </div>
+        })}
     </div>
 </main>
 }
 
 export { Product }
+
