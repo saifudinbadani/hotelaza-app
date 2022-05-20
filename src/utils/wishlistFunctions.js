c9bf4bbba0914c3131a8ef653000dcb469a59244
Saifudin Badani <87968258+saifudinbadani@users.noreply.github.com>
2022-05-20 19:25:53 +0800

    chore: converted wishlist fn to api call

--- src/utils/wishlistFunctions.js
+++ src/utils/wishlistFunctions.js
@@ -1,4 +1,60 @@
 import { products } from "../backend/db/products";
+import axios from 'axios';
+
+
+export const getWishlistApiCall = async (encodedToken) => {
+    try {
+        const response = await axios.get('/api/user/wishlist', {
+            headers : {
+                authorization: encodedToken
+            }
+        })
+        if (response.status === 200 || response.status === 201) {
+            return response.data.wishlist;
+        }
+    } catch (error) {
+        console.log(error)
+    } 
+}
+
+
+export const addToWishlistApiCall = async (product, encodedToken) => {
+
+    try {
+        const response = await axios.post('/api/user/wishlist', {
+            product
+        },{
+            headers : {
+                authorization: encodedToken
+            }
+        })
+        if (response.status === 200 || response.status === 201) {
+            return response.data.wishlist;
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+
+export const removeFromWishlistApiCall = async (id, encodedToken) => {
+
+    try {
+        const response = await axios.delete(`/api/user/wishlist/${id}`,{
+            headers: {
+                authorization: encodedToken
+            }
+        })
+        if (response.status === 200 || response.status === 201) {
+            return response.data.wishlist;
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
 
 export const addToWishlist = (cartState, action) => {
     if (cartState.wishlist.includes(action.payload)) {
